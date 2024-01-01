0c1a937f78d8f9b2c1457fd8f91e57eb5cb09d50
Muhammad Farabie <farabie024@gmail.com>
2024-01-01 20:24:13 +0700

    increment dan decrement

---
+++ bin/11.Operator_Penugasan/increment_decrement.dart
@@ -0,0 +1,18 @@
+void main() {
+  //Increment
+  var angka1 = 10;
+  var angka2 = 10;
+
+  print(angka1);
+  angka1++; //angka1 = angka1 + 1 (angka1)
+  print(angka1);
+  ++angka2; // angka2 = angka2 + 1 (angka2 + 1);
+  print(angka2);
+
+  //Decrement
+
+  var x = 17;
+  print(x);
+  print(--x); //x = x - 1(x -1)
+  print(x--); // x = x -1 (x)
+}
---
+++ bin/11.Operator_Penugasan/operator_penugasan.dart
@@ -0,0 +1,32 @@
+void main() {
+  //Aritmatika
+  //a = a + 10
+
+  //Penugasan
+  //a+= 10
+
+  var a = 2;
+
+  a += 10;
+  print(a);
+
+  //Aritmatika
+  //b = a - 10
+
+  //Penugasan
+  //b-= 10
+
+  var b = 2;
+  b -= 10;
+  print(b);
+
+  //Perkalian
+  var c = 3;
+  c *= 3;
+  print(c);
+
+  //Pembagian
+  var d = 4;
+  d ~/= 5;
+  print(d);
+}
