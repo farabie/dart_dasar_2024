f0e82ed9489e4aed4f6f19b7396d2aad1bd84d4c
Muhammad Farabie <farabie024@gmail.com>
2024-01-01 18:42:51 +0700

    Menggabungkan String

---
+++ bin/6.String/menggabungkan_string.dart
@@ -0,0 +1,12 @@
+void main() {
+  String firstName = 'Muhammad ';
+  String lastName = 'Farabie ';
+
+  String fullName = firstName + lastName;
+  print(fullName);
+  print(firstName + lastName);
+
+  //Atau bisa juga dengan menggabungkan string dengan menggunakan spasi
+  String bendaRumah = 'setrika ' 'Botol Minum ' ' Meja';
+  print(bendaRumah);
+}
