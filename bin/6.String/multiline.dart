c0e778d5e4b93088780b702ac6bab356e965d528
Muhammad Farabie <farabie024@gmail.com>
2024-01-01 18:44:56 +0700

    multiline

---
+++ bin/6.String/multiline.dart
@@ -0,0 +1,8 @@
+void main() {
+  String multiLine = '''
+  Ini Merupakan String Multi line jadi jika anda membutuhkan string yang cukup panjang
+  bisa menggunakan string ini
+  ''';
+
+  print(multiLine);
+}
