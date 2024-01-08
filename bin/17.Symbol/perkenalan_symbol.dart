eda33ac017ae624c46914a046fe2cdce683b6d71
Muhammad Farabie <farabie024@gmail.com>
2024-01-09 06:09:11 +0700

    symbol

---
+++ bin/17.Symbol/perkenalan_symbol.dart
@@ -0,0 +1,9 @@
+void main() {
+  //Symbol ini berfungsi jika kita ingin membuat kata yanga ada "" atau #
+
+  Symbol symbol = Symbol("Farabie");
+  var symbol2 = "#belajar2";
+
+  print(symbol);
+  print(symbol2);
+}
