d1fca3dd461adc477f78fe1808ab34cb46d28f67
Muhammad Farabie <farabie024@gmail.com>
2024-01-15 21:29:39 +0700

    For Dasar

---
+++ bin/23.For/perkenalan_for.dart
@@ -0,0 +1,12 @@
+void main() {
+  //For merupakan salah satu kata kunci untuk melakukan perulangan
+
+  //For(init statement; kondisi; post statement)
+
+  /**
+   * Init statement akan dieksekusi hanya sekali diawal sebelum perulangan
+   * Kondisi akan dilakukan pengecekan di setiap perulangan
+   * dieksekusi setiap kali diakhiri perulangan
+   * Init statement, kondisi, post statement itu tidak wajib diisi
+   */
+}
---
+++ bin/23.For/perulangan_dengan_kondisi.dart
@@ -0,0 +1,8 @@
+void main() {
+  int i = 1;
+
+  for (; i <= 10;) {
+    print("Perulangan Ke $i");
+    i++;
+  }
+}
