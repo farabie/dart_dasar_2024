e7346c44642d1ee8b41da55578666fa482696404
Muhammad Farabie <farabie024@gmail.com>
2024-01-18 16:31:46 +0700

    For In

---
+++ bin/27.For_In/perkenalan_for_in.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Fungsi For In biasanya digunakan untuk memecahkan suatu array
+
+  var arrayNama = ["Nama1", "Nama2", "Nama3", "Nama4", ];
+
+  //For tanpa menggunakan for in
+  for(var i = 0; i < arrayNama.length; i++) {
+    print(arrayNama[i]);
+  }
+}
