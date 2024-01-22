19eb28a9b4b306834cf490ad587fbf5e46619519
Muhammad Farabie <farabie024@gmail.com>
2024-01-22 10:56:10 +0700

    Main Function

---
+++ bin/35.Main_Function/main_function.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  /**
+   * Main function memiliki sebuah parameter optional, yaitu adalah arguments, dimana data parameter tersebut berupa List<String>
+    Data List<String> tersebut diambil secara otomatis ketika kita menjalankan file dart menggunakan perintah  :
+    dart namafile.dart arg1 arg2 arg3
+    dart namafile.dart  argument 1 argument  2 argument 3
+   */
+  print("Args: $args");
+}
